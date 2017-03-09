50b3ff6e57544509a6a4138a1a8008d541e4efc0
Vadim <dnzl@bk.ru>
2017-03-10 02:01:23 +0300

    Changed CountWords

--- src/main/java/track/lessons/lesson1/CountWords.java
+++ src/main/java/track/lessons/lesson1/CountWords.java
@@ -1,25 +1,26 @@
 package track.lessons.lesson1;
 
+import java.util.Scanner;
 import java.io.File;
+import java.io.*;
 
 /**
  * Задание 1: Реализовать два метода
- *
+ * <p>
  * Формат файла: текстовый, на каждой его строке есть (или/или)
  * - целое число (int)
  * - текстовая строка
  * - пустая строка (пробелы)
- *
- *
+ * <p>
+ * <p>
  * Пример файла - words.txt в корне проекта
- *
+ * <p>
  * ******************************************************************************************
- *  Пожалуйста, не меняйте сигнатуры методов! (название, аргументы, возвращаемое значение)
- *
- *  Можно дописывать новый код - вспомогательные методы, конструкторы, поля
- *
+ * Пожалуйста, не меняйте сигнатуры методов! (название, аргументы, возвращаемое значение)
+ * <p>
+ * Можно дописывать новый код - вспомогательные методы, конструкторы, поля
+ * <p>
  * ******************************************************************************************
- *
  */
 public class CountWords {
 
@@ -27,11 +28,25 @@ public class CountWords {
      * Метод на вход принимает объект File, изначально сумма = 0
      * Нужно пройти по всем строкам файла, и если в строке стоит целое число,
      * то надо добавить это число к сумме
+     *
      * @param file - файл с данными
      * @return - целое число - сумма всех чисел из файла
      */
     public long countNumbers(File file) throws Exception {
-        return 0;
+        Scanner scanIn = new Scanner(file);
+        long sumInt = 0;
+        String bufString;
+        while (scanIn.hasNext()) {
+
+            if (scanIn.hasNextInt()) {
+                sumInt += scanIn.nextInt();
+            } else if (scanIn.hasNextLine()) {
+                bufString = scanIn.nextLine();
+            }
+
+        }
+        scanIn.close();
+        return sumInt;
     }
 
 
@@ -39,11 +54,33 @@ public class CountWords {
      * Метод на вход принимает объект File, изначально результат= ""
      * Нужно пройти по всем строкам файла, и если в строка не пустая и не число
      * то надо присоединить ее к результату через пробел
+     *
      * @param file - файл с данными
      * @return - результирующая строка
      */
     public String concatWords(File file) throws Exception {
-        return null;
-    }
+        Scanner scanIn = new Scanner(file);
+        StringBuilder builder = new StringBuilder();
+        String bufString;
+        int bufNumber;
+        while (scanIn.hasNext()) {
 
+            if (scanIn.hasNextInt()) {
+                bufNumber = scanIn.nextInt();
+            } else {
+                bufString = scanIn.nextLine().trim();
+                if (!bufString.equals("\n") & !bufString.equals("")) {
+                    builder.append(" ");
+                    builder.append(bufString);
+                }
+            }
+
+        }
+
+        String sumString = builder.toString().trim();
+        scanIn.close();
+        return sumString;
+    }
 }
+
+
